9639ae11677ade74a84557d324ebb97a908c2fab
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-02-12 22:52:46 +0300

    Update display_image.cpp

--- samples/cpp/tutorial_code/introduction/display_image/display_image.cpp
+++ samples/cpp/tutorial_code/introduction/display_image/display_image.cpp
@@ -11,29 +11,29 @@ using namespace cv;
 int main()
 {
     //! [imread]
-    std::string image_path = samples::findFile("starry_night.jpg");
+    String image_path = samples::findFile("starry_night.jpg");
     Mat img = imread(image_path, IMREAD_COLOR);
     //! [imread]
 
-    //! [empty]
+    //! [is_empty]
     if(img.empty())
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-    //! [empty]
+    //! [is_empty]
 
     //! [imshow]
     imshow("Display window", img);
     int k = waitKey(0); // Wait for a keystroke in the window
     //! [imshow]
 
-    //! [imsave]
+    //! [imwrite]
     if(k == 's')
     {
         imwrite("starry_night.png", img);
     }
-    //! [imsave]
+    //! [imwrite]
 
     return 0;
 }

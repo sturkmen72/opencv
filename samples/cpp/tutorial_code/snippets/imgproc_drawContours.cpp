2ec76e6d1cb12344344f26bdbf514e1c820c2d54
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-03-12 07:43:42 +0300

    Update imgproc_drawContours.cpp

--- samples/cpp/tutorial_code/snippets/imgproc_drawContours.cpp
+++ samples/cpp/tutorial_code/snippets/imgproc_drawContours.cpp
@@ -9,13 +9,12 @@ int main( int argc, char** argv )
     Mat src;
     // the first command-line parameter must be a filename of the binary
     // (black-n-white) image
-    if( argc != 2 || !(src=imread(argv[1], 0)).data)
+    if( argc != 2 || !(src=imread(argv[1], IMREAD_GRAYSCALE)).data)
         return -1;
 
     Mat dst = Mat::zeros(src.rows, src.cols, CV_8UC3);
 
     src = src > 1;
-    namedWindow( "Source", 1 );
     imshow( "Source", src );
 
     vector<vector<Point> > contours;
@@ -30,10 +29,9 @@ int main( int argc, char** argv )
     for( ; idx >= 0; idx = hierarchy[idx][0] )
     {
         Scalar color( rand()&255, rand()&255, rand()&255 );
-        drawContours( dst, contours, idx, color, FILLED, 8, hierarchy );
+        drawContours( dst, contours, idx, color, FILLED, LINE_8, hierarchy );
     }
 
-    namedWindow( "Components", 1 );
     imshow( "Components", dst );
     waitKey(0);
 }

475de61aa9c56e0f7c04635ace78833c5dc9b8b1
ocpalo <iamberkeyavas@gmail.com>
2022-08-07 17:32:58 +0300

    rgb->gray typo

--- modules/imgcodecs/src/grfmt_spng.cpp
+++ modules/imgcodecs/src/grfmt_spng.cpp
@@ -577,12 +577,12 @@ void spngCvt_BGR2Gray_8u_C3C1R( const uchar* bgr, int bgr_step,
     int i;
     for( ; size.height--; gray += gray_step )
     {
+        double cBGR0 = 0.1140;
+        double cBGR2 = 0.2990;
+        if (_swap_rb) std::swap(cBGR0, cBGR2);
         for( i = 0; i < size.width; i++, bgr += 3 )
         {
-            double cBGR2 = 0.1140;
-            double cBGR0 = 0.2990;
-            if (_swap_rb) std::swap(cBGR0, cBGR2);
-            int t = cBGR2 * bgr[0] + 0.5870 * bgr[1] + cBGR0 * bgr[2];
+            int t = static_cast<int>(cBGR0 * bgr[0] + 0.5870 * bgr[1] + cBGR2 * bgr[2]);
             gray[i] = (uchar)t;
         }
 
@@ -597,12 +597,12 @@ void spngCvt_BGRA2Gray_8u_C4C1R( const uchar* bgra, int rgba_step,
     int i;
     for( ; size.height--; gray += gray_step )
     {
-        double cBGR2 = 0.1140;
-        double cBGR0 = 0.2990;
+        double cBGR0 = 0.1140;
+        double cBGR2 = 0.2990;
         if (_swap_rb) std::swap(cBGR0, cBGR2);
         for( i = 0; i < size.width; i++, bgra += 4 )
         {
-            int t = cBGR2 * bgra[0] + 0.5870 * bgra[1] + cBGR0 * bgra[2];
+            int t = cBGR0 * bgra[0] + 0.5870 * bgra[1] + cBGR2 * bgra[2];
             gray[i] = (uchar)t;
         }
 
@@ -617,12 +617,12 @@ void spngCvt_BGRA2Gray_16u_CnC1R( const ushort* bgr, int bgr_step,
     int i;
     for( ; size.height--; gray += gray_step )
     {
+        double cBGR0 = 0.1140;
+        double cBGR2 = 0.2990;
+        if (_swap_rb) std::swap(cBGR0, cBGR2);
         for( i = 0; i < size.width; i++, bgr += ncn )
         {
-            double cBGR2 = 0.1140;
-            double cBGR0 = 0.2990;
-            if (_swap_rb) std::swap(cBGR0, cBGR2);
-            int t = cBGR2 * bgr[0] + 0.5870 * bgr[1] + cBGR0 * bgr[2];
+            int t = cBGR0 * bgr[0] + 0.5870 * bgr[1] + cBGR2 * bgr[2];
             gray[i] = (ushort)t;
         }
 

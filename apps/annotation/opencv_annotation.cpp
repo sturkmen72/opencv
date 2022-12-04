4b33e077fcefe027d38aecc0555e187f18ea8c88
Suleyman TURKMEN <sturkmen@hotmail.com>
2022-12-05 00:01:58 +0300

    Update opencv_annotation.cpp

--- apps/annotation/opencv_annotation.cpp
+++ apps/annotation/opencv_annotation.cpp
@@ -20,9 +20,13 @@ int main(int argc, char** argv)
     vconcat(SplicingImage, SplicingImage, SplicingImage);
     Mat mask = 255 * Mat::ones(inImage.rows, inImage.cols, inImage.depth());
     Point centerPt = Point(SplicingImage.cols / 2, SplicingImage.rows / 2);
+    TickMeter tm1;
+    tm1.start();
     seamlessClone(inImage, SplicingImage, mask, centerPt, SplicingImage, NORMAL_CLONE);
+    tm1.stop();
     tm.stop();
     imwrite("cloned1.png", SplicingImage);
+    cout << "seamlessClone time :" << tm1 << endl;
     cout << tm << endl;
 
     cv::resize(img0, inImage, Size(936, 936)); //Set the size of the input image
@@ -31,8 +35,12 @@ int main(int argc, char** argv)
     hconcat(inImage, inImage, SplicingImage);
     vconcat(SplicingImage, SplicingImage, SplicingImage);
     Mat mask2 = 255 * Mat::ones(inImage.rows, inImage.cols, inImage.depth());
+    tm1.reset();
+    tm1.start();
     seamlessClone(inImage, SplicingImage, mask2, centerPt, SplicingImage, NORMAL_CLONE);
+    tm1.stop();
     tm.stop();
+    cout << "seamlessClone time :" << tm1 << endl;
     cout << tm << endl;
     imwrite("cloned2.png", SplicingImage);
     return 0;

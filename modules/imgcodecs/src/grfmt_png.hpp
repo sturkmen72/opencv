43a31d4e5442490951cec270c4b9053c03dfea70
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-06-07 20:06:32 +0300

    Update grfmt_png.hpp

--- modules/imgcodecs/src/grfmt_png.hpp
+++ modules/imgcodecs/src/grfmt_png.hpp
@@ -86,6 +86,7 @@ public:
 
     bool  isFormatSupported( int depth ) const CV_OVERRIDE;
     bool  write( const Mat& img, const std::vector<int>& params ) CV_OVERRIDE;
+    bool writemulti(const std::vector<Mat>& img_vec, const std::vector<int>& params) CV_OVERRIDE;
 
     ImageEncoder newEncoder() const CV_OVERRIDE;
 
@@ -95,6 +96,33 @@ protected:
 };
 
 }
+
+struct CHUNK { unsigned char* p; unsigned int size; };
+struct Image
+{
+    typedef unsigned char* ROW;
+    unsigned int w, h, bpp, delay_num, delay_den;
+    unsigned char* p;
+    ROW* rows;
+    Image() : w(0), h(0), bpp(0), delay_num(1), delay_den(10), p(0), rows(0) { }
+    ~Image() { }
+    void init(unsigned int w1, unsigned int h1, unsigned int bpp1)
+    {
+        w = w1; h = h1; bpp = bpp1;
+        int rowbytes = w * bpp;
+        rows = new ROW[h];
+        rows[0] = p = new unsigned char[h * rowbytes];
+        for (unsigned int j = 1; j < h; j++)
+            rows[j] = rows[j - 1] + rowbytes;
+    }
+    void free() { delete[] rows; delete[] p; }
+};
+
+struct APNGFrame { unsigned char* p, ** rows; unsigned int w, h, delay_num, delay_den; };
+
+int load_apng(std::string inputFileName, std::vector<Image>& img);
+void save_strip_png(std::string outFileName, std::vector<Image>& imgs);
+int save_apng(char* szOut, std::vector<APNGFrame>& frames, unsigned int first, unsigned int loops, unsigned int coltype, int deflate_method, int iter);
 
 #endif
 

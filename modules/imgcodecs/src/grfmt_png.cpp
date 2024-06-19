bb2059fd531c17332973c109c47cba1f1aac04f9
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-06-19 03:28:57 +0300

    Update grfmt_png.cpp

--- modules/imgcodecs/src/grfmt_png.cpp
+++ modules/imgcodecs/src/grfmt_png.cpp
@@ -1,10 +1,48 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level
-// directory of this distribution and at http://opencv.org/license.html
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//                For Open Source Computer Vision Library
+//
+// Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
+// Copyright (C) 2009, Willow Garage Inc., all rights reserved.
+// Third party copyrights are property of their respective owners.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//   * Redistribution's of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//   * Redistribution's in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//   * The name of the copyright holders may not be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holders and contributors "as is" and
+// any express or implied warranties, including, but not limited to, the implied
+// warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the Intel Corporation or contributors be liable for any direct,
+// indirect, incidental, special, exemplary, or consequential damages
+// (including, but not limited to, procurement of substitute goods or services;
+// loss of use, data, or profits; or business interruption) however caused
+// and on any theory of liability, whether in contract, strict liability,
+// or tort (including negligence or otherwise) arising in any way out of
+// the use of this software, even if advised of the possibility of such damage.
+//
+//M*/
 
 #include "precomp.hpp"
 
-#ifdef HAVE_PNG
+#ifdef HAVE_PNG_TEST
 
 /****************************************************************************************\
     This part of the file implements PNG codec on base of libpng library,
@@ -12,34 +50,6 @@
     (see otherlibs/_graphics/readme.txt for copyright notice)
     and png2bmp sample from libpng distribution (Copyright (C) 1999-2001 MIYASAKA Masaru)
 \****************************************************************************************/
-
-//this file includes some part of APNG Disassembler 2.9 and APNG Optimizer 1.4 
-
-/* APNG Disassembler 2.9
- *
- * Deconstructs APNG files into individual frames.
- *
- * http://apngdis.sourceforge.net
- *
- * Copyright (c) 2010-2017 Max Stepin
- * maxst at users.sourceforge.net
- *
- * zlib license
- * ------------
-*/
-
-/* APNG Optimizer 1.4
- *
- * Makes APNG files smaller.
- *
- * http://sourceforge.net/projects/apng/files
- *
- * Copyright (c) 2011-2015 Max Stepin
- * maxst at users.sourceforge.net
- *
- * zlib license
- * ------------
- */
 
 #ifndef _LFS64_LARGEFILE
 #  define _LFS64_LARGEFILE 0
@@ -437,18 +447,6 @@ bool  PngEncoder::write( const Mat& img, const std::vector<int>& params )
     if(f) fclose( (FILE*)f );
 
     return result;
-}
-
-bool PngEncoder::writemulti(const std::vector<Mat>& img_vec, const std::vector<int>& params)
-{
-    // ******  WIP *****
-    // ****** test *****
-    std::vector<Image> imgs;
-
-    int r1 = load_apng(m_filename, imgs);
-    save_apng("test_new"+m_filename, imgs, 0, 0, 6, 0, 0);
-    printf("writemulti %d",r1);
-    return true;
 }
 
 }

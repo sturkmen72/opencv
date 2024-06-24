be82e5a805d22ab4ae588bbedf077e924a61abed
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-06-24 12:25:51 +0300

    Update grfmt_png.hpp

--- modules/imgcodecs/src/grfmt_png.hpp
+++ modules/imgcodecs/src/grfmt_png.hpp
@@ -1,62 +1,186 @@
-/*M///////////////////////////////////////////////////////////////////////////////////////
-//
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-//
-//  By downloading, copying, installing or using the software you agree to this license.
-//  If you do not agree to this license, do not download, install,
-//  copy or use the software.
-//
-//
-//                           License Agreement
-//                For Open Source Computer Vision Library
-//
-// Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
-// Copyright (C) 2009, Willow Garage Inc., all rights reserved.
-// Third party copyrights are property of their respective owners.
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//   * Redistribution's of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//
-//   * Redistribution's in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//
-//   * The name of the copyright holders may not be used to endorse or promote products
-//     derived from this software without specific prior written permission.
-//
-// This software is provided by the copyright holders and contributors "as is" and
-// any express or implied warranties, including, but not limited to, the implied
-// warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corporation or contributors be liable for any direct,
-// indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level
+// directory of this distribution and at http://opencv.org/license.html
 
-#ifndef _GRFMT_PNG_H_
-#define _GRFMT_PNG_H_
+/****************************************************************************\
+ *
+ *  this file includes some modified part of apngasm and APNG Optimizer 1.4
+ *  both have zlib license.
+ *
+ ****************************************************************************/
+
+
+ /*  apngasm
+ *
+ *  The next generation of apngasm, the APNG Assembler.
+ *  The apngasm CLI tool and library can assemble and disassemble APNG image files.
+ *
+ *  https://github.com/apngasm/apngasm
+
+
+ /* APNG Optimizer 1.4
+ *
+ * Makes APNG files smaller.
+ *
+ * http://sourceforge.net/projects/apng/files
+ *
+ * Copyright (c) 2011-2015 Max Stepin
+ * maxst at users.sourceforge.net
+ *
+ * zlib license
+ * ------------
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+
+#ifndef _GRFMT_APNG_H_
+#define _GRFMT_APNG_H_
 
 #ifdef HAVE_PNG
 
 #include "grfmt_base.hpp"
 #include "bitstrm.hpp"
+#include <png.h>
+#include <zlib.h>
 
 namespace cv
 {
 
-class PngDecoder CV_FINAL : public BaseImageDecoder
+struct CHUNK { unsigned char* p; unsigned int size; };
+struct OP { unsigned char* p; unsigned int size; int x, y, w, h, valid, filters; };
+
+const unsigned DEFAULT_FRAME_NUMERATOR = 100; //!< @brief The default numerator for the frame delay fraction.
+const unsigned DEFAULT_FRAME_DENOMINATOR =  1000; //!< @brief The default denominator for the frame delay fraction.
+
+typedef struct {
+    unsigned char r, g, b;
+} rgb;
+
+typedef struct {
+    unsigned char r, g, b, a;
+} rgba;
+
+
+class APNGFrame
+{
+public:
+    APNGFrame();
+    virtual ~APNGFrame() {}
+
+    /**
+     * @brief Creates an APNGFrame from a bitmapped array of RBGA pixel data.
+     * @param pixels The RGBA pixel data.
+     * @param width The width of the pixel data.
+     * @param height The height of the pixel data.
+     * @param delayNum The delay numerator for this frame (defaults to DEFAULT_FRAME_NUMERATOR).
+     * @param delayDen The delay denominator for this frame (defaults to DEFAULT_FRAME_DENMINATOR).
+     */
+    APNGFrame(rgba* pixels, int width, int height,
+        int delayNum = DEFAULT_FRAME_NUMERATOR,
+        int delayDen = DEFAULT_FRAME_DENOMINATOR);
+
+    /**
+     * @brief Creates an APNGFrame from a PNG file.
+     * @param filePath The relative or absolute path to an image file.
+     * @param delayNum The delay numerator for this frame (defaults to DEFAULT_FRAME_NUMERATOR).
+     * @param delayDen The delay denominator for this frame (defaults to DEFAULT_FRAME_DENMINATOR).
+     */
+    APNGFrame(const std::string& filePath,
+        int delayNum = DEFAULT_FRAME_NUMERATOR,
+        int delayDen = DEFAULT_FRAME_DENOMINATOR);
+
+    /**
+     * @brief Creates an APNGFrame from a bitmapped array of RBG pixel data.
+     * @param pixels The RGB pixel data.
+     * @param width The width of the pixel data.
+     * @param height The height of the pixel data.
+     * @param delayNum The delay numerator for this frame (defaults to DEFAULT_FRAME_NUMERATOR).
+     * @param delayDen The delay denominator for this frame (defaults to DEFAULT_FRAME_DENMINATOR).
+     */
+    APNGFrame(rgb* pixels, int width, int height,
+        int delayNum = DEFAULT_FRAME_NUMERATOR,
+        int delayDen = DEFAULT_FRAME_DENOMINATOR);
+
+    /**
+     * @brief Creates an APNGFrame from a bitmapped array of RBG pixel data.
+     * @param pixels The RGB pixel data.
+     * @param width The width of the pixel data.
+     * @param height The height of the pixel data.
+     * @param trns_color An array of transparency data.
+     * @param delayNum The delay numerator for this frame (defaults to DEFAULT_FRAME_NUMERATOR).
+     * @param delayDen The delay denominator for this frame (defaults to DEFAULT_FRAME_DENMINATOR).
+     */
+    APNGFrame(rgb* pixels, int width, int height,
+        rgb* trns_color = NULL, int delayNum = DEFAULT_FRAME_NUMERATOR,
+        int delayDen = DEFAULT_FRAME_DENOMINATOR);
+
+    /**
+     * @brief Saves this frame as a single PNG file.
+     * @param outPath The relative or absolute path to save the image file to.
+     * @return Returns true if save was successful.
+     */
+    bool save(const std::string& outPath) const;
+
+    int width() const { return m_width; }
+    int height() const { return m_height; }
+    virtual int type() const { return m_type; }
+
+    // Raw pixel data
+    uchar* pixels(uchar* setPixels = NULL);
+
+    // Palette into
+    rgb* palette(rgb* setPalette = NULL);
+    rgb _palette[256];
+
+    // Transparency info
+    uchar* transparency(uchar* setTransparency = NULL);
+
+    // Sizes for palette and transparency records
+    int paletteSize(int setPaletteSize = 0);
+    int transparencySize(int setTransparencySize = 0);
+
+    // Delay is numerator/denominator ratio, in seconds
+    int delayNum(int setDelayNum = 0);
+    int delayDen(int setDelayDen = 0);
+    uchar** rows(uchar** setRows = NULL);
+
+
+protected:
+    uchar* m_pixels;
+    int  m_width;
+    int  m_height;
+    int  m_type;
+    int  m_color_type;
+    int _paletteSize;
+    int _transparencySize;
+    uchar _transparency[256];
+    int _delayNum;
+    int _delayDen;
+    uchar** _rows;
+};
+
+
+class ApngDecoder CV_FINAL : public BaseImageDecoder
 {
 public:
 
-    PngDecoder();
-    virtual ~PngDecoder();
+    ApngDecoder();
+    virtual ~ApngDecoder();
 
     bool  readData( Mat& img ) CV_OVERRIDE;
     bool  readHeader() CV_OVERRIDE;
@@ -66,6 +190,12 @@ public:
 
 protected:
 
+    unsigned int read_chunk(FILE* f, CHUNK* pChunk);
+    int processing_start(png_structp& png_ptr, png_infop& info_ptr, void* frame_ptr, bool hasInfo, CHUNK& chunkIHDR, std::vector<CHUNK>& chunksInfo);
+    int processing_data(png_structp png_ptr, png_infop info_ptr, unsigned char* p, unsigned int size);
+    int processing_finish(png_structp png_ptr, png_infop info_ptr);
+    void compose_frame(unsigned char** rows_dst, unsigned char** rows_src, unsigned char bop, unsigned int x, unsigned int y, unsigned int w, unsigned int h);
+    int load_apng(std::string inputFileName, std::vector<APNGFrame>& frames, unsigned int& first, unsigned int& loops);
     static void readDataFromBuf(void* png_ptr, uchar* dst, size_t size);
 
     int   m_bit_depth;
@@ -75,23 +205,53 @@ protected:
     FILE* m_f;
     int   m_color_type;
     size_t m_buf_pos;
+    bool m_is_animated;
+    int m_loops;
 };
 
 
-class PngEncoder CV_FINAL : public BaseImageEncoder
+class ApngEncoder CV_FINAL : public BaseImageEncoder
 {
 public:
-    PngEncoder();
-    virtual ~PngEncoder();
+    ApngEncoder();
+    virtual ~ApngEncoder();
 
-    bool  isFormatSupported( int depth ) const CV_OVERRIDE;
-    bool  write( const Mat& img, const std::vector<int>& params ) CV_OVERRIDE;
+    bool isFormatSupported( int depth ) const CV_OVERRIDE;
+    bool write( const Mat& img, const std::vector<int>& params ) CV_OVERRIDE;
+    bool writemulti(const std::vector<Mat>& img_vec, const std::vector<int>& params) CV_OVERRIDE;
+    size_t save_apng(std::string inputFileName, std::vector<APNGFrame>& frames, unsigned int first, unsigned int loops, unsigned int coltype, int deflate_method, int iter);
+    void optim_dirty(std::vector<APNGFrame>& frames);
+    void optim_duplicates(std::vector<APNGFrame>& frames, unsigned int first);
 
     ImageEncoder newEncoder() const CV_OVERRIDE;
 
 protected:
     static void writeDataToBuf(void* png_ptr, uchar* src, size_t size);
     static void flushBuf(void* png_ptr);
+
+private:
+    void write_chunk(FILE* f, const char* name, unsigned char* data, unsigned int length);
+    void write_IDATs(FILE* f, int frame, unsigned char* data, unsigned int length, unsigned int idat_size);
+    void process_rect(unsigned char* row, int rowbytes, int bpp, int stride, int h, unsigned char* rows);
+    void deflate_rect_fin(int deflate_method, int iter, unsigned char* zbuf, unsigned int* zsize, int bpp, int stride, unsigned char* rows, int zbuf_size, int n);
+    void deflate_rect_op(unsigned char* pdata, int x, int y, int w, int h, int bpp, int stride, int zbuf_size, int n);
+    void get_rect(unsigned int w, unsigned int h, unsigned char* pimage1, unsigned char* pimage2, unsigned char* ptemp, unsigned int bpp, unsigned int stride, int zbuf_size, unsigned int has_tcolor, unsigned int tcolor, int n);
+
+    void (*process_callback)(float);
+    unsigned char* op_zbuf1;
+    unsigned char* op_zbuf2;
+    z_stream       op_zstream1;
+    z_stream       op_zstream2;
+    unsigned char* row_buf;
+    unsigned char* sub_row;
+    unsigned char* up_row;
+    unsigned char* avg_row;
+    unsigned char* paeth_row;
+    OP             op[6];
+    rgb            palette[256];
+    unsigned char  trns[256];
+    unsigned int   palsize, trnssize;
+    unsigned int   next_seq_num;
 };
 
 }

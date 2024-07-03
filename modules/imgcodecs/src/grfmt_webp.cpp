1c8a916b49ea0b3698dbeaa5d42de3028588e738
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-07-03 22:26:04 +0300

    WebP animation support

--- modules/imgcodecs/src/grfmt_webp.cpp
+++ modules/imgcodecs/src/grfmt_webp.cpp
@@ -1,44 +1,6 @@
-/*M///////////////////////////////////////////////////////////////////////////////////////
-//
-//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-//
-//  By downloading, copying, installing or using the software you agree to this license.
-//  If you do not agree to this license, do not download, install,
-//  copy or use the software.
-//
-//
-//                           License Agreement
-//                For Open Source Computer Vision Library
-//
-// Copyright (C) 2000-2008, Intel Corporation, all rights reserved.
-// Copyright (C) 2009, Willow Garage Inc., all rights reserved.
-// Third party copyrights are property of their respective owners.
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//   * Redistribution's of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//
-//   * Redistribution's in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
-//
-//   * The name of the copyright holders may not be used to endorse or promote products
-//     derived from this software without specific prior written permission.
-//
-// This software is provided by the copyright holders and contributors "as is" and
-// any express or implied warranties, including, but not limited to, the implied
-// warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the Intel Corporation or contributors be liable for any direct,
-// indirect, incidental, special, exemplary, or consequential damages
-// (including, but not limited to, procurement of substitute goods or services;
-// loss of use, data, or profits; or business interruption) however caused
-// and on any theory of liability, whether in contract, strict liability,
-// or tort (including negligence or otherwise) arising in any way out of
-// the use of this software, even if advised of the possibility of such damage.
-//
-//M*/
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level
+// directory of this distribution and at http://opencv.org/license.html
 
 #ifdef HAVE_WEBP
 
@@ -46,14 +8,11 @@
 
 #include <webp/decode.h>
 #include <webp/encode.h>
-
+#include <webp/demux.h>
+#include <webp/mux.h>
 #include <stdio.h>
 #include <limits.h>
-
 #include "grfmt_webp.hpp"
-
-#include "opencv2/imgproc.hpp"
-
 #include <opencv2/core/utils/configuration.private.hpp>
 
 namespace cv
@@ -102,6 +61,7 @@ ImageDecoder WebPDecoder::newDecoder() const
 
 bool WebPDecoder::readHeader()
 {
+    anim_decoder = NULL;
     uint8_t header[WEBP_HEADER_SIZE] = { 0 };
     if (m_buf.empty())
     {
@@ -126,8 +86,24 @@ bool WebPDecoder::readHeader()
     WebPBitstreamFeatures features;
     if (VP8_STATUS_OK == WebPGetFeatures(header, sizeof(header), &features))
     {
-        CV_CheckEQ(features.has_animation, 0, "WebP backend does not support animated webp images");
+        m_has_animation = features.has_animation > 0;
+        if (m_has_animation)
+        {
+            WebPData webp_data;
+            webp_data.bytes = (const uint8_t*)data.ptr();
+            webp_data.size = data.total();
 
+            WebPAnimDecoderOptions dec_options;
+            WebPAnimDecoderOptionsInit(&dec_options);
+            dec_options.color_mode = MODE_BGRA;
+            anim_decoder = WebPAnimDecoderNew(&webp_data, &dec_options);
+
+            if (anim_decoder == NULL)
+            {
+                fprintf(stderr, "Error parsing image");
+                return false;
+            }
+        }
         m_width  = features.width;
         m_height = features.height;
 
@@ -155,7 +131,7 @@ bool WebPDecoder::readData(Mat &img)
     CV_CheckEQ(img.cols, m_width, "");
     CV_CheckEQ(img.rows, m_height, "");
 
-    if (m_buf.empty())
+    if (data.empty())
     {
         fs.seekg(0, std::ios::beg); CV_Assert(fs && "File stream error");
         data.create(1, validateToInt(fs_size), CV_8UC1);
@@ -168,7 +144,7 @@ bool WebPDecoder::readData(Mat &img)
     {
         Mat read_img;
         CV_CheckType(img.type(), img.type() == CV_8UC1 || img.type() == CV_8UC3 || img.type() == CV_8UC4, "");
-        if (img.type() != m_type)
+        if (img.type() != m_type || img.cols != m_width || img.rows != m_height)
         {
             read_img.create(m_height, m_width, m_type);
         }
@@ -181,6 +157,19 @@ bool WebPDecoder::readData(Mat &img)
         size_t out_data_size = read_img.dataend - out_data;
 
         uchar *res_ptr = NULL;
+ 
+        if (m_has_animation)
+        {
+            uint8_t* buf;
+            int timestamp;
+
+            WebPAnimDecoderGetNext(anim_decoder, &buf, &timestamp);
+            Mat tmp(Size(m_width, m_height), CV_8UC4, buf);
+            tmp.copyTo(img);
+
+            return true;
+        }
+
         if (channels == 3)
         {
             CV_CheckTypeEQ(read_img.type(), CV_8UC3, "");
@@ -227,6 +216,12 @@ bool WebPDecoder::readData(Mat &img)
         }
     }
     return true;
+}
+
+bool WebPDecoder::nextPage()
+{
+    // Prepare the next page, if any.
+    return WebPAnimDecoderHasMoreFrames(anim_decoder) > 0;
 }
 
 WebPEncoder::WebPEncoder()
@@ -329,6 +324,90 @@ bool WebPEncoder::write(const Mat& img, const std::vector<int>& params)
     }
 
     return size > 0;
+}
+
+bool WebPEncoder::writemulti(const std::vector<Mat>& img_vec, const std::vector<int>& params)
+{
+    int ok =0;
+    int duration = 100;
+    int timestamp_ms = 0;
+    const int width = img_vec[0].cols, height = img_vec[0].rows;
+
+    WebPAnimEncoderOptions anim_config;
+    WebPConfig config;
+    WebPPicture pic;
+    WebPData webp_data;
+
+    WebPDataInit(&webp_data);
+    if (!WebPAnimEncoderOptionsInit(&anim_config) ||
+        !WebPConfigInit(&config) ||
+        !WebPPictureInit(&pic)) {
+        fprintf(stderr, "Library version mismatch!\n");
+        goto End;
+    }
+
+    config.lossless = 1;
+    config.quality = 100.0f;
+
+    if (params.size() > 1)
+    {
+        if (params[0] == IMWRITE_WEBP_QUALITY)
+        {
+            config.lossless = 0;
+            config.quality = static_cast<float>(params[1]);
+            if (config.quality < 1.0f)
+            {
+                config.quality = 1.0f;
+            }
+            if (config.quality > 100.0f)
+            {
+                config.lossless = 1;
+            }
+        }
+    }
+
+    anim_encoder = WebPAnimEncoderNew(width, height, &anim_config);
+
+    pic.width = width;
+    pic.height = height;
+    pic.use_argb = 1;
+    pic.argb_stride = width;
+    WebPEncode(&config, &pic);
+
+    for (size_t i = 0; i < img_vec.size(); i++)
+    {
+        pic.argb = (uint32_t*)img_vec[i].data;
+        ok = WebPAnimEncoderAdd(anim_encoder, &pic, timestamp_ms, &config);
+        timestamp_ms += duration;
+    }
+
+    // add a last fake frame to signal the last duration
+    ok = ok && WebPAnimEncoderAdd(anim_encoder, NULL, timestamp_ms, NULL);
+    ok = ok && WebPAnimEncoderAssemble(anim_encoder, &webp_data);
+
+End:
+    if (ok)
+    {
+        if (m_buf)
+        {
+            m_buf->resize(webp_data.size);
+            memcpy(&(*m_buf)[0], webp_data.bytes, webp_data.size);
+        }
+        else
+        {
+            FILE* fd = fopen(m_filename.c_str(), "wb");
+            if (fd != NULL)
+            {
+                fwrite(webp_data.bytes, webp_data.size, sizeof(uint8_t), fd);
+                fclose(fd); fd = NULL;
+            }
+        }
+    }
+
+    // free resources
+    WebPAnimEncoderDelete(anim_encoder);
+    WebPDataClear(&webp_data);
+    return ok > 0;
 }
 
 }

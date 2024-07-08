6e2559348038b85aea7372b340097c711f1e4e63
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-07-08 18:29:24 +0300

    Update webp_test.cpp

--- cmake/checks/webp_test.cpp
+++ cmake/checks/webp_test.cpp
@@ -3,6 +3,8 @@
 int main()
 {
   WebPAnimEncoderOptions anim_config;
+  WebPAnimEncoderOptionsInit(&anim_config);
   WebPAnimEncoder* anim_encoder = WebPAnimEncoderNew(320, 240, &anim_config);
+  WebPAnimEncoderDelete(anim_encoder);
   return 0;
 }

9aa751759757134ca21b748bdcaea562a403be48
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-09-25 15:15:22 +0300

    Update imgcodecs.hpp

--- modules/imgcodecs/include/opencv2/imgcodecs.hpp
+++ modules/imgcodecs/include/opencv2/imgcodecs.hpp
@@ -72,7 +72,6 @@ enum ImreadModes {
        IMREAD_COLOR                = 1,  //!< Same as IMREAD_COLOR_BGR.
        IMREAD_ANYDEPTH             = 2,  //!< If set, return 16-bit/32-bit image when the input has the corresponding depth, otherwise convert it to 8-bit.
        IMREAD_ANYCOLOR             = 4,  //!< If set, the image is read in any possible color format.
-       IMREAD_UNCHANGED_APPLY_EXIF = 7,
        IMREAD_LOAD_GDAL            = 8,  //!< If set, use the gdal driver for loading the image.
        IMREAD_REDUCED_GRAYSCALE_2  = 16, //!< If set, always convert image to the single channel grayscale image and the image size reduced 1/2.
        IMREAD_REDUCED_COLOR_2      = 17, //!< If set, always convert image to the 3 channel BGR color image and the image size reduced 1/2.
@@ -81,6 +80,7 @@ enum ImreadModes {
        IMREAD_REDUCED_GRAYSCALE_8  = 64, //!< If set, always convert image to the single channel grayscale image and the image size reduced 1/8.
        IMREAD_REDUCED_COLOR_8      = 65, //!< If set, always convert image to the 3 channel BGR color image and the image size reduced 1/8.
        IMREAD_IGNORE_ORIENTATION   = 128, //!< If set, do not rotate the image according to EXIF's orientation flag.
+       IMREAD_APPLY_ORIENTATION    = ~IMREAD_IGNORE_ORIENTATION,
        IMREAD_COLOR_RGB            = 256, //!< If set, always convert image to the 3 channel RGB color image.
      };
 
